d10c4c401a4ec9dcc23622f3f3f73968c34777c5
calahill37 <calahill@iu.edu>
2023-03-19 20:41:08 -0400

    Added code to look at the 50 highest and lowest fitness genes per treatment

--- Scripts/Rscripts/Test_PCoA_relativeabundance_top50.R
+++ Scripts/Rscripts/Test_PCoA_relativeabundance_top50.R
@@ -11,6 +11,8 @@ colnames(metadata) <- c("compartment","Ntreatment")
 library(vegan)
 library(ggplot2)
 library(stringr)
+library(tidyverse)
+library(dplyr)
 
 rel.dat = decostand(rbdat,method="total")
 
@@ -58,25 +60,75 @@ cleannames #these are meant to be passed through ordilabel() to create cleaner l
 
 rbrar <- rarecurve(rbdat, step = 500, sample = raremax, xlab = "Total barcodes", ylab = "Unique barcodes", col = "blue", cex = 0.5, label = FALSE)
 
-
 rbrar
-
-gene_list <- scores(pcoa,display="all")
 
 
 # the following is a ChatGPT-generated script to extract the 50 most abundant genes per experiment... wow!! 
-top_bc <- apply(rbdat, 1, function(x) {
+
+#top_bc <- apply(rbdat, 1, function(x) {
+#  sorted <- sort(x, decreasing = TRUE)
+#  names(sorted)[1:50]
+#})
+
+# Order the top species by their abundance
+#ordered_top_bc <- apply(rbdat, 1, function(x) {
+#  sorted <- sort(x, decreasing = TRUE)
+#  names(sorted)[1:50][order(sorted[1:50], decreasing = TRUE)]
+#})
+
+# Print the ordered top species for each row (site)
+#print(ordered_top_bc)
+
+#end of the generated portion; however, note that I worked with + modified this script in order to produce the following code
+
+
+fitdat = read.delim("C:/Users/User/Desktop/thesis_stuff/R_working/fitness_data/html/SmeliPlant/fit_logratios.tab", row.names = 1)
+# fitdat = read.delim("~/GitHub/rhizo.rb.tnseq/Data/fitness_data/html/SmeliPlant/fit_logratios.tab", row.names = 1)
+                    
+# Extract the top 50 species for each column (species)
+top_fit <- apply(fitdat, 2, function(x) {
   sorted <- sort(x, decreasing = TRUE)
   names(sorted)[1:50]
 })
 
 # Order the top species by their abundance
-ordered_top_bc <- apply(rbdat, 1, function(x) {
+ordered_top_fit <- apply(fitdat, 2, function(x) {
   sorted <- sort(x, decreasing = TRUE)
   names(sorted)[1:50][order(sorted[1:50], decreasing = TRUE)]
 })
 
-# Print the ordered top species for each row (site)
-print(ordered_top_bc)
+orderedtopfit <- ordered_top_fit[,-1:-2]
 
-#end of the generated portion
+# Print the ordered top species for each column (species)
+print(orderedtopfit)
+
+bottom50genes <- t(orderedtopfit) #the bc insertionally inactivates the gene, so more bc in the t1 sample = beneficial inactivation = lower fitness
+
+lowest50fit <- cbind(metadata,bottom50genes) #generates list of 50 genes with lowest fitness values per treatment, with metadata
+
+#the following code will produce a list of the 50 genes with the highest fitness
+
+# Extract the bottom 50 species for each column (species)
+bottom_fit <- apply(fitdat, 2, function(x) {
+  sorted <- sort(x)
+  names(sorted)[1:50]
+})
+
+# Order the bottom species by their abundance
+ordered_bottom_fit <- apply(fitdat, 2, function(x) {
+  sorted <- sort(x)
+  names(sorted)[1:50][order(sorted[1:50])]
+})
+
+orderedbottomfit <- ordered_bottom_fit[,-1:-2]
+
+# Print the ordered bottom species for each column (species)
+print(orderedbottomfit)
+
+top50genes <- t(orderedbottomfit)
+
+top50fit <- cbind(metadata,top50genes) #like lowest50 but with the highest fitness
+
+# would like to find a way to make a table of mean gene counts per treatment, then top/bottom mean gene fitness lists per treatment
+# tried summarise and group_by with a cbind(metadata,rbdat) dataframe, but haven't figured out how to automate over all 5200 columns; might be able to rotate table
+# once we have mean count data, should be really easy to generate meaned versions of top50fit, lowest50fit if desirable
