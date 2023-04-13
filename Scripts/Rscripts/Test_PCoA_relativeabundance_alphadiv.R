6a8c89120dce06c4c98ca65da7e87051d3e71935
calahill37 <calahill@iu.edu>
2023-04-13 02:12:33 -0400

    Added alpha diversity measures, summary stats, tweaks to lm to remove date variable

--- Scripts/Rscripts/Test_PCoA_relativeabundance_alphadiv.R
+++ Scripts/Rscripts/Test_PCoA_relativeabundance_alphadiv.R
@@ -24,7 +24,7 @@ pcoa.sites = scores(pcoa,display = "sites") #see loadings, takes awhile
 pcoa.sites = as.data.frame(scores(pcoa,display = "sites"))
 
 ggplot(pcoa.sites,aes(x=MDS1,y=MDS2,color=metadata$compartment,shape=metadata$Ntreatment)) +
-  geom_point()+
+  geom_point(size=8)+
   xlab("PCo 1 (25%)") +
   ylab("PCo 2 (14%)") +
   theme_bw(base_size = 30) +
@@ -50,15 +50,21 @@ Srare
 
 labelnames <- data.frame(rownames(rbdat)) #make smaller labels for rarefaction curve 
 
-shortnames <- str_sub(labelnames$rownames.rbdat., 7, 20)
+shortnames <- str_sub(labelnames$rownames.rbdat., 7, 22)
 
 shortnames
 
 cleannames <- gsub('\\.', ' ',shortnames)
 
+numbersonly <- substring(labelnames$rownames.rbdat., 7, 9)
+
 cleannames #these are meant to be passed through ordilabel() to create cleaner labels for the rarefaction curve but I'm still working on the syntax
 
-rbrar <- rarecurve(rbdat, step = 500, sample = raremax, xlab = "Total barcodes", ylab = "Unique barcodes", col = "blue", cex = 0.5, label = FALSE)
+rownames(rbdat) <- numbersonly
+
+cols <- c("darkred", "darkred", "darkred", "darkred", "forestgreen", "forestgreen", "forestgreen", "forestgreen", "forestgreen", "blue", "blue", "blue", "blue", "purple", "purple", "purple", "orange", "orange","orange","orange","orange")
+
+rbrar <- rarecurve(rbdat, step = 500, sample = 1000000, xlab = "Total barcodes", ylab = "Unique barcodes", col = cols, cex = 0.5, label = TRUE)
 
 rbrar
 
@@ -82,9 +88,13 @@ rbrar
 #end of the generated portion; however, note that I worked with + modified this script in order to produce the following code
 
 
-fitdat = read.delim("C:/Users/User/Desktop/thesis_stuff/R_working/fitness_data/html/SmeliPlant/fit_logratios.tab", row.names = 1)
+fitdat = t(read.delim("C:/Users/User/Desktop/thesis_stuff/R_working/fitness_data/html/SmeliPlant/fit_logratios_clean.tab", row.names = 1))
 # fitdat = read.delim("~/GitHub/rhizo.rb.tnseq/Data/fitness_data/html/SmeliPlant/fit_logratios.tab", row.names = 1)
-                    
+
+cleanrbdat = t(read.delim("C:/Users/User/Desktop/thesis_stuff/R_working/fitness_data/html/SmeliPlant/gene_counts_clean.tab", row.names = 1))
+
+goodcountdat <- subset(cleanrbdat, row.names(fitdat) %in% c("X21", "X22", "X24", "X26", "X27", "X28", "X29", "X30", "X32", "X33", "X35", "X37", "X40", "X41", "X42", "X43", "X44", "X45"))
+
 # Extract the top 50 species for each column (species)
 top_fit <- apply(fitdat, 2, function(x) {
   sorted <- sort(x, decreasing = TRUE)
@@ -129,6 +139,61 @@ top50genes <- t(orderedbottomfit)
 
 top50fit <- cbind(metadata,top50genes) #like lowest50 but with the highest fitness
 
-# would like to find a way to make a table of mean gene counts per treatment, then top/bottom mean gene fitness lists per treatment
-# tried summarise and group_by with a cbind(metadata,rbdat) dataframe, but haven't figured out how to automate over all 5200 columns; might be able to rotate table
-# once we have mean count data, should be really easy to generate meaned versions of top50fit, lowest50fit if desirable
+# write.csv(lowest50fit, "C:/Users/User/Desktop/lowest50fitgenes.csv", row.names = TRUE)
+
+# write.csv(top50fit, "C:/Users/User/Desktop/top50fitgenes.csv", row.names = TRUE)
+
+
+#this part of the script generates alpha diversity data for the library mutant based on the data that passes the vertical line test
+
+shannonfit <- numeric(nrow(goodcountdat))
+for(j in 1:nrow(goodcountdat)) {
+  shannonfit[j] <- vegan::diversity(goodcountdat[j,], index = "shannon")
+}
+
+shannonfit #shannon diversity of lib
+
+Evar <- function(x) {
+  x <- as.vector(x[x > 0])
+  1 - (2/pi)*atan(var(log(x)))
+} #this function calculates the Smith and Wilson's Evenness for the given site
+
+Evarfit <- numeric(nrow(goodcountdat))
+for(j in 1:nrow(goodcountdat)) {
+  Evarfit[j] <- Evar(goodcountdat[j,])
+}
+
+Evarfit #evenness of lib in each pot
+
+countzeros <- function(row) {
+  numzeros <- sum(row == 0)
+  percentzeros <- 100 - (numzeros / length(row) * 100)
+  return(percentzeros)
+} #returns the percentage of species found in each site
+
+#calculates % found for each row (site)
+percentfoundfit <- apply(goodcountdat, 1, countzeros)
+
+percentfoundfit
+
+S_obs <- function(x = ""){
+  rowSums(x>0)*1
+} #observed species richness in a given site
+
+richnessfit <-  S_obs(goodcountdat)
+
+richnessfit
+
+alphadiversitylib = as.data.frame(matrix(ncol=4,nrow=nrow(goodcountdat)))
+colnames(alphadiversitylib) <- c('Shannon diversity','Evenness (SW)','% Found','Richness')
+rownames(alphadiversitylib) <- rownames(goodcountdat)
+
+alphadiversitylib[,1] <- shannonfit
+
+alphadiversitylib[,2] <- Evarfit
+
+alphadiversitylib[,3] <- percentfoundfit
+
+alphadiversitylib[,4] <- richnessfit
+
+alphadiversitylib #final collated dataframe of alpha diversity measures by pot
